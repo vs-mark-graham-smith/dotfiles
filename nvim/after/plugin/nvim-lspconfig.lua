c63ae6c1f813f81dd41e4cb8ca4f8e53a4828e16
Mark Graham-Smith <markgraham-smith@Marks-MacBook-Pro-2.local>
2022-12-26 18:44:10 +0000

    Update

--- nvim/after/plugin/nvim-lspconfig.lua
+++ nvim/after/plugin/nvim-lspconfig.lua
@@ -1,2 +1,7 @@
 require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.intelephense.setup {}
+require'lspconfig'.intelephense.setup {
+    on_attach = function(client, bufnr)
+        vim.cmd('syntax on')
+    end
+}
+require'lspconfig'.vuels.setup{}
